93ce36cea5d52832037379ee5d92d30d27c9af55
Mark Sayers <marksayers46@icloud.com>
2022-04-06 17:46:28 -0500

    setup dependencies

--- src/components/Veggie.jsx
+++ src/components/Veggie.jsx
@@ -2,15 +2,28 @@ import { useState, useEffect } from "react";
 
 const Veggie = () => {
 
-  const [veggie, setveggie] = useState([]);
+  const [veggie, setVeggie] = useState([]);
+
+  useEffect(() => {
+    getVeggie();
+  }, []);
 
   const getVeggie = async () => {
-    const api = await fetch(``)
+    const api = await fetch(`https://api.spoonacular.com/recipes/random?apiKey=${process.env.REACT_APP_API_KEY}&number=10`);
+    const data =await api.json();
+    console.log(data);
+    setVeggie(data.recipes)
   } 
   
   return (
     <div>
-      
+      {veggie.map((recipe) => {
+        return (
+          <div key={recipe.id}>
+            <p>{recipe.title}</p>
+          </div>
+        );
+      })};
     </div>
   )
 }
