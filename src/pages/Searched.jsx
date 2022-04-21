6dcfae64326626e71ce4c404ceb20d4990c480d5
Mark Sayers <marksayers46@icloud.com>
2022-04-21 16:38:07 -0500

    finished links to recipe page

--- src/pages/Searched.jsx
+++ src/pages/Searched.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { useState, useEffect } from 'react';
-import { useParams } from 'react-router-dom';
+import { useParams, Link } from 'react-router-dom';
 import styled from 'styled-components';
 
 
@@ -26,8 +26,10 @@ const Searched = () => {
         {searchedRecipes.map((item) => {
             return(
                 <Card key={item.id}>
-                    <img src={item.image} alt="" />
-                    <h4>{item.title}</h4>
+                    <Link to={'/recipe/' + searchedRecipes.id}>
+                        <img src={item.image} alt="" />
+                        <h4>{item.title}</h4>
+                    </Link>
                 </Card>
             )
         })}
