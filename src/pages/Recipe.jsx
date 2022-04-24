01c51907a789b962fdfc60a9e1f8ee374ab820f2
Mark Sayers <marksayers46@icloud.com>
2022-04-24 12:36:41 -0500

    styled Recipe details page

--- src/pages/Recipe.jsx
+++ src/pages/Recipe.jsx
@@ -5,7 +5,6 @@ import { useParams } from 'react-router-dom';
 
 const Recipe = () => {
     const [details, setDetails] = useState({});
-    const [activeTab, setActiveTab] = useState("instructions");
     const params = useParams();
 
     const fetchDetails = async () => {
@@ -20,10 +19,56 @@ const Recipe = () => {
    
     
   return (
-    <div>
-        <h2>{details.title}</h2>
-    </div>
+    <detailWrapper>  
+        <div>
+            <h2>{details.title}</h2>
+            <img src={details.image} alt="" />
+        </div>
+        <Info>
+            <Button>Instructions</Button>
+            <Button>Ingredients</Button>
+        </Info>
+    </detailWrapper>
+    
   )
 }
+
+
+const detailWrapper = styled.div`
+    margin-top: 10rem;
+    margin-bottom: 5rem;
+    display: flex;
+
+    .active {
+        background: linear-gradient(35deg, #494949, #313131);
+        color: white;
+    }
+
+    h2 {
+        margin-bottom: 2rem;
+    }
+
+    li {
+        font-size: 1.2rem;
+        line-height: 2.5rem;
+    }
+
+    ul {
+        margin-top: 2rem;
+    }
+`;
+
+const Button = styled.button`
+    padding: 1rem 2rem;
+    color: #313131;
+    background: white;
+    border: 2px solid black;
+    margin-right: 2rem;
+    font-weight: 600;
+`;
+
+const Info = styled.div`
+    margin-left: 10rem;
+`;
 
 export default Recipe;
